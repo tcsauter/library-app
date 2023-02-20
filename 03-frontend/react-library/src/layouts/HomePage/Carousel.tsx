6a42e03dcc695e40fd6fe8582da937ac88fe128b
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-02-20 14:45:38 -0500

    Finish up Carousel component (Part 2).

--- 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -80,6 +80,27 @@ export const Carousel = () => {
                         <span className='visually-hidden'>Next</span>
                     </button>
                 </div>
+
+                {/* Mobile */}
+                <div className='d-lg-none mt-3'>
+                    <div className='row d-flex justify-content-center align-items-center'>
+                        <div className='text-center'>
+                            <img
+                                src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                width='151'
+                                height='233'
+                                alt='book'
+                            />
+                            <h6 className='mt-2'><b>Book</b></h6>
+                            <p>Luv2Code</p>
+                            <a className='btn main-color text-white' href='#'>Reserve</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='homepage-carousel-title mt-3'>
+                    <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
+                </div>
+
             </div>
         </div>
     );
