ec9c0616f95b01de3b4c5f43ea34e14eb3b57124
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-03-31 07:49:44 -0400

    Pagination fix. Add method to find books by title.

--- 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -12,12 +12,20 @@ export const SearchBooksPage = () => {
     const [booksPerPage] =  useState(5);
     const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
+    const [search, setSearch] = useState("");
+    const [searchUrl, setSearchUrl] = useState("");
 
     useEffect(() => {
         const fetchBooks = async () => {
             const baseUrl: string = "http://localhost:8080/api/books"
 
-            const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`
+            let url: string;
+
+            if (searchUrl === '') {
+                url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+            } else {
+                url = baseUrl + searchUrl;
+            }
 
             const response = await fetch(url);
 
@@ -54,7 +62,8 @@ export const SearchBooksPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         });
-    }, [])
+        window.scrollTo(0, 0)
+    }, [booksPerPage, currentPage, searchUrl])
 
     if(isLoading){
         return(
@@ -68,6 +77,14 @@ export const SearchBooksPage = () => {
                 <p>{httpError}</p>
             </div>
         );
+    }
+
+    const searchHandleChange = () => {
+        if (search === '') {
+            setSearchUrl('')
+        } else {
+            setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`)
+        }
     }
 
     const indexOfLastBook: number = currentPage * booksPerPage;
@@ -86,8 +103,9 @@ export const SearchBooksPage = () => {
                             <div className='d-flex'>
                                 <input className='form-control me-2' type='search'
                                        placeholder='search' aria-label='Search'
+                                       onChange={e => setSearch(e.target.value)}
                                 />
-                                <button className='btn btn-outline-success'>
+                                <button className='btn btn-outline-success' onClick={() => searchHandleChange()}>
                                     Search
                                 </button>
                             </div>
@@ -130,10 +148,10 @@ export const SearchBooksPage = () => {
                         </div>
                     </div>
                     <div className='mt-3'>
-                        <h5>Number of results: (22)</h5>
+                        <h5>Number of results: ({totalAmountOfBooks})</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items:
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks}
                     </p>
                     {books.map(book => (
                         <SearchBook book={book} key={book.id} />
