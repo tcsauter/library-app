4173276b178cfa01b10283e1b77e711a148fb02d
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-04-12 07:08:59 -0400

    Complete BookCheckoutPage and add StarsReview and CheckoutAndReviewBox.

--- 03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,4 +1,5 @@
 import BookModel from "../../../models/BookModel";
+import {Link} from "react-router-dom";
 
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
 
@@ -51,9 +52,9 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
                     </div>
                 </div>
                 <div className='col-md-4 d-flex justify-content-center align-items-center'>
-                    <a className='btn btn-md main-color text-white' href='#'>
+                    <Link className='btn btn-md main-color text-white' to={`/checkout/${props.book.id}`}>
                         View Details
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
